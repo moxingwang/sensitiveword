6664963cf9e02c4fa2c05c3a613a9e3e5833ecd8
xingwang.mo <xingwang.mo@chinaredstar.com>
2019-07-29 22:11:54 +0800

    add

---
+++ src/main/java/top/moxingwang/sensitiveword/SensitivewordApplication.java
@@ -0,0 +1,13 @@
+package top.moxingwang.sensitiveword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensitivewordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensitivewordApplication.class, args);
+	}
+
+}

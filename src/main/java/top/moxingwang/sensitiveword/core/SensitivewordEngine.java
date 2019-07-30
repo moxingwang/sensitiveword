f868d76317a33bded8d7910903bec149aad38dc4
moxingwang <xingwang.mo@chinaredstar.com>
2019-07-30 17:15:28 +0800

    q

--- src/main/java/top/moxingwang/sensitiveword/core/SensitivewordEngine.java
+++ src/main/java/top/moxingwang/sensitiveword/core/SensitivewordEngine.java
@@ -4,17 +4,15 @@ package top.moxingwang.sensitiveword.core;
  * @description:
  * @author: MoXingwang 2019-07-29 13:09
  **/
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
 
-public class SensitivewordEngine
-{
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class SensitivewordEngine {
     /**
-     * 敏感词库
+     * 敏感词库,可以存储多个业务场景(桶)
      */
-    public static Map sensitiveWordMap = null;
+    public static Map<Integer,Map<String,String>> lexicon = new ConcurrentHashMap<>();
 
     /**
      * 只过滤最小敏感词
@@ -26,18 +24,61 @@ public class SensitivewordEngine
      */
     public static int maxMatchType = 2;
 
+
+    /**
+     * 封装敏感词库
+     */
+    @SuppressWarnings("rawtypes")
+    private void addSensitiveWordToHashMap(Integer bucketId, Set<String> keyWordSet) {
+        // 初始化HashMap对象并控制容器的大小
+        HashMap<String,String> sensitiveWordMap = new HashMap(keyWordSet.size());
+        // 敏感词
+        String key = null;
+        // 用来按照相应的格式保存敏感词库数据
+        Map nowMap = null;
+        // 用来辅助构建敏感词库
+        Map<String, String> newWorMap = null;
+        // 使用一个迭代器来循环敏感词集合
+        Iterator<String> iterator = keyWordSet.iterator();
+        while (iterator.hasNext()) {
+            key = iterator.next();
+            // 等于敏感词库，HashMap对象在内存中占用的是同一个地址，所以此nowMap对象的变化，sensitiveWordMap对象也会跟着改变
+            nowMap = sensitiveWordMap;
+            for (int i = 0; i < key.length(); i++) {
+                // 截取敏感词当中的字，在敏感词库中字为HashMap对象的Key键值
+                char keyChar = key.charAt(i);
+
+                // 判断这个字是否存在于敏感词库中
+                Object wordMap = nowMap.get(keyChar);
+                if (wordMap != null) {
+                    nowMap = (Map) wordMap;
+                } else {
+                    newWorMap = new HashMap<String, String>();
+                    newWorMap.put("isEnd", "0");
+                    nowMap.put(keyChar, newWorMap);
+                    nowMap = newWorMap;
+                }
+
+                // 如果该字是当前敏感词的最后一个字，则标识为结尾字
+                if (i == key.length() - 1) {
+                    nowMap.put("isEnd", "1");
+                }
+            }
+        }
+
+        lexicon.put(bucketId,sensitiveWordMap);
+    }
+
     /**
      * 敏感词库敏感词数量
      *
      * @return
      */
-    public static int getWordSize()
-    {
-        if (SensitivewordEngine.sensitiveWordMap == null)
-        {
+    public static int getWordSize(String bucketId) {
+        if (lexicon.get(bucketId) == null) {
             return 0;
         }
-        return SensitivewordEngine.sensitiveWordMap.size();
+        return lexicon.get(bucketId).size();
     }
 
     /**
@@ -47,14 +88,11 @@ public class SensitivewordEngine
      * @param matchType
      * @return
      */
-    public static boolean isContaintSensitiveWord(String txt, int matchType)
-    {
+    public static boolean isContaintSensitiveWord(String txt, int matchType) {
         boolean flag = false;
-        for (int i = 0; i < txt.length(); i++)
-        {
+        for (int i = 0; i < txt.length(); i++) {
             int matchFlag = checkSensitiveWord(txt, i, matchType);
-            if (matchFlag > 0)
-            {
+            if (matchFlag > 0) {
                 flag = true;
             }
         }
@@ -68,15 +106,12 @@ public class SensitivewordEngine
      * @param matchType
      * @return 敏感词内容
      */
-    public static Set<String> getSensitiveWord(String txt, int matchType)
-    {
+    public static Set<String> getSensitiveWord(String txt, int matchType) {
         Set<String> sensitiveWordList = new HashSet<String>();
 
-        for (int i = 0; i < txt.length(); i++)
-        {
+        for (int i = 0; i < txt.length(); i++) {
             int length = checkSensitiveWord(txt, i, matchType);
-            if (length > 0)
-            {
+            if (length > 0) {
                 // 将检测出的敏感词保存到集合中
                 sensitiveWordList.add(txt.substring(i, i + length));
                 i = i + length - 1;
@@ -94,15 +129,13 @@ public class SensitivewordEngine
      * @param replaceChar
      * @return
      */
-    public static String replaceSensitiveWord(String txt, int matchType, String replaceChar)
-    {
+    public static String replaceSensitiveWord(String txt, int matchType, String replaceChar) {
         String resultTxt = txt;
         Set<String> set = getSensitiveWord(txt, matchType);
         Iterator<String> iterator = set.iterator();
         String word = null;
         String replaceString = null;
-        while (iterator.hasNext())
-        {
+        while (iterator.hasNext()) {
             word = iterator.next();
             replaceString = getReplaceChars(replaceChar, word.length());
             resultTxt = resultTxt.replaceAll(word, replaceString);
@@ -118,11 +151,9 @@ public class SensitivewordEngine
      * @param length
      * @return
      */
-    private static String getReplaceChars(String replaceChar, int length)
-    {
+    private static String getReplaceChars(String replaceChar, int length) {
         String resultReplace = replaceChar;
-        for (int i = 1; i < length; i++)
-        {
+        for (int i = 1; i < length; i++) {
             resultReplace += replaceChar;
         }
 
@@ -137,42 +168,35 @@ public class SensitivewordEngine
      * @param matchType
      * @return
      */
-    public static int checkSensitiveWord(String txt, int beginIndex, int matchType)
-    {
+    public static int checkSensitiveWord(String txt, int beginIndex, int matchType) {
         boolean flag = false;
         // 记录敏感词数量
         int matchFlag = 0;
         char word = 0;
         Map nowMap = SensitivewordEngine.sensitiveWordMap;
-        for (int i = beginIndex; i < txt.length(); i++)
-        {
+        for (int i = beginIndex; i < txt.length(); i++) {
             word = txt.charAt(i);
             // 判断该字是否存在于敏感词库中
             nowMap = (Map) nowMap.get(word);
-            if (nowMap != null)
-            {
+            if (nowMap != null) {
                 matchFlag++;
                 // 判断是否是敏感词的结尾字，如果是结尾字则判断是否继续检测
-                if ("1".equals(nowMap.get("isEnd")))
-                {
+                if ("1".equals(nowMap.get("isEnd"))) {
                     flag = true;
                     // 判断过滤类型，如果是小过滤则跳出循环，否则继续循环
-                    if (SensitivewordEngine.minMatchTYpe == matchType)
-                    {
+                    if (SensitivewordEngine.minMatchTYpe == matchType) {
                         break;
                     }
                 }
-            }
-            else
-            {
+            } else {
                 break;
             }
         }
-        if (!flag)
-        {
+        if (!flag) {
             matchFlag = 0;
         }
         return matchFlag;
     }
+
 
 }

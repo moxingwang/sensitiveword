f34482502f9d66de23833efa19e916122d1cd7d6
moxingwang <xingwang.mo@chinaredstar.com>
2019-07-31 10:25:06 +0800

    q

--- src/test/java/top/moxingwang/sensitiveword/Test.java
+++ src/test/java/top/moxingwang/sensitiveword/Test.java
@@ -2,9 +2,7 @@ package top.moxingwang.sensitiveword;
 
 import top.moxingwang.sensitiveword.core.SensitivewordEngine;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 /**
@@ -14,20 +12,20 @@ import java.util.Set;
 public class Test {
     public static void main(String[] args) {
         Set<String> list1 = new HashSet<>();
-        for (int i = 0; i < 100000; i++) {
-            list1.add(i+"铭感词"+i);
-        }
-
+        list1.add("回东");
+        list1.add("我的");
+        list1.add("的");
 
         Set<String> list2 = new HashSet<>();
         list2.add("你的");
 
 
+        SensitivewordEngine.add(1, list1);
+        SensitivewordEngine.add(2, list2);
 
-        SensitivewordEngine.add(1,list1);
-        SensitivewordEngine.add(2,list2);
+        System.out.println(SensitivewordEngine.getSensitiveWord(1, "我的哈哈返我的543回东萨", 1));
 
-        System.out.println(SensitivewordEngine.getSensitiveWord(1,"我的哈哈返我的543回东萨",2));;
-        System.out.println(SensitivewordEngine.getSensitiveWord(2,"我的哈哈返我的543回东萨",2));;
+        System.out.println(SensitivewordEngine.getSensitiveWord(2, "我的哈哈返我的543回东萨", 1));
+
     }
 }
